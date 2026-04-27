bc73085588f13d27a78245f7233deaccd7b4007b
Joecode404 <joe.coleman907@gmail.com>
2026-04-27 10:34:43 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -109,4 +109,6 @@ demo = gr.Interface(
 
 # RUN APP
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    import os
+port = int(os.environ.get("PORT", 7860))
+demo.launch(server_name="0.0.0.0", server_port=port)
